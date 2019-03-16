7153f0d91e10ca7615a89d38c1637ecd6f340c2c
Larry Schirmer <larryschirmer@gmail.com>
2019-03-16 10:23:19 -0400

    setup project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createGlobalStyle } from "styled-components";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import MainPage from "./MainPage";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export const colorMain = "#FFF";
+const GlobalStyle = createGlobalStyle`
+  body,
+  html {
+    width: 100%;
+    height: 100%;
+    background: ${colorMain};
+    font-family: 'Open Sans', sans-serif;
+    color: #1a1a1a;
+    font-size: 10px;
+  }
+`;
+
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <MainPage />
+  </>,
+  document.getElementById("root")
+);
